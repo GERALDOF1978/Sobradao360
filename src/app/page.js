fb2cb5fe91c68abafde94bc69b19df91203a4db6
GERALDOF1978 <geraldof1978@gmail.com>
2026-09-18 14:47:45 +0000

    1

---
+++ src/app/page.js
@@ -0,0 +1,6 @@
+<h1 className="text-5xl font-extrabold text-blue-900 tracking-tight">
+  Sobradão 360
+</h1>
+<p className="text-xl text-gray-600">
+  Tudo sobre a nossa região: notícias, guia comercial e conexão entre vizinhos em todas as direções.
+</p>
