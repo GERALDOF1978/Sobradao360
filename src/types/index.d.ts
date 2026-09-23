1c2d4b9ad18348bc4b69d87e99ebdcc2b3140a3a
GERALDOF1978 <geraldof1978@gmail.com>
2026-09-23 12:24:26 +0000

    Corrigindo tipos d.ts e ativando o Tailwind v4 moderno

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,17 +1,3 @@
-mkdir -p src/types
-cat << 'EOF' > src/types/index.d.ts
-declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
-}
-
-declare module '*.svg' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.png' {
-  const content: string;
-  export default content;
-}
-EOF
+declare module "*.css";
+declare module "*.svg";
+declare module "*.png";
