0e349148461f0d685095bcee79b27a26d44b1999
GERALDOF1978 <geraldof1978@gmail.com>
2026-09-23 17:20:29 +0000

    8

---
+++ src/globals.d.ts
@@ -0,0 +1,6 @@
+// Declarações globais para evitar erros de tipo no TypeScript
+declare module "*.css";
+declare module "*.scss";
+
+declare module "@/context/AuthContext";
+declare module "@/lib/firebase";
